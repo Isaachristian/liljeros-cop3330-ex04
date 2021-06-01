e51948df852b970874a0e4690e298926899672d4
Isaac Liljeros <isaak.liljeros@gmail.com>
2021-06-01 19:26:55 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        String noun, verb, adjective, adverb;
+
+        System.out.print("Enter a noun: ");
+        noun = scanner.nextLine();
+        System.out.print("Enter a verb: ");
+        verb = scanner.nextLine();
+        System.out.print("Enter a adjective: ");
+        adjective = scanner.nextLine();
+        System.out.print("Enter a adverb: ");
+        adverb = scanner.nextLine();
+
+        System.out.printf("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb);
+    }
+}
